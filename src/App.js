55b1bf2280bba6305b4294bf188c75049c658d23
Krzysztof Dyner <krisdyner31@gmail.com>
2021-04-19 11:47:31 +0100

    adressing errors from netlify

--- src/App.js
+++ src/App.js
@@ -112,11 +112,11 @@ function ChatRoom() {
 
 function ChatMessage(props) {
   const {text, uid, photoURL} = props.message;
-  const messageClass = uid == auth.currentUser.uid ? 'sent' : 'received';
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
 
   return (
     <div className={`message ${messageClass}`}>
-      <img src={photoURL}/>
+      <img src={photoURL} alt="photoURL"/>
       <p>{text}</p>
     </div>
   )
